8ba3ff3f85d0614ab1715a49c1cb22f963602aab
bobwei <bob61016@gmail.com>
2017-01-22 02:51:17 +0800

    add filter function and update photo size

--- src/modules/pages/home/containers/Home/mapStateToProps.js
+++ src/modules/pages/home/containers/Home/mapStateToProps.js
@@ -1,11 +1,19 @@
 import R from 'ramda';
 import { get } from 'lodash/fp';
+import { getFormValues } from 'redux-form';
 
 const mapStateToProps = state => ({
   data: R.compose(
+    R.filter(R.anyPass([
+      R.compose(R.isNil, R.always(getFormValues('dataQuery')(state))),
+      R.compose(
+        R.test(new RegExp(R.prop('q')((getFormValues('dataQuery')(state) || {})), 'i')),
+        R.prop('title'),
+      ),
+    ])),
     R.map(row => ({
       ...R.pick(['id', 'title'])(row),
-      photo: get('apps[0].thumbnails.medium.url')(row),
+      photo: get('apps[0].thumbnails.small.url')(row),
       link: R.compose(
         R.join(''),
         R.prepend('http://www.viveport.com/apps/'),
